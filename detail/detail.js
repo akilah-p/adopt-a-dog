00197da1a06429e25c8b409fca519542ffb22f40
Akilah Powell <atpjun@gmail.com>
2022-11-14 20:00:14 -0400

    created detail.js load page so that dog details load.

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,18 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
+window.addEventListener('load', async () => {
+    const params = new URLSearchParams(window.location.search);
+
+    const id = params.get('id');
+
+
+
+    const details = await getDog(id);
+
+    const getDetailEl = renderDogDetail(details);
+    dogDetailContainer.append(getDetailEl);
+});
 // get the id from URL
 // use the id to fetch the dog
 // render and append this dog's details to the container
